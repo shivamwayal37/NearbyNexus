182b0e6052d4132e49ea22b9fcef23582e29ff78
Shivam Wayal <148749349+wizard734@users.noreply.github.com>
2025-07-26 12:28:51 +0530

    frontend and backend initialized

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl text-amber-400 underline'>
+      hello World
+    </div>
+  )
+}
+
+export default App
